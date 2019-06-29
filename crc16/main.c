6d11306416845f75adc169bcf005da0141478f78
Kai Scheffer <kai@sffr.de>
2019-06-29 19:02:42 +0200

    Initial Commit

---
+++ crc16/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  crc16
+//
+//  Created by Kai Scheffer on 29.06.19.
+//  Copyright © 2019 Kai Scheffer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
